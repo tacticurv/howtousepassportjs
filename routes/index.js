36c8c3001d6a8e8348f62bf73f820b6e81b5cc9c
tacticurv <tacticurv@gmail.com>
2015-04-24 05:51:49 +0530

    Revert "Revert "PassportJS Integration""
    This reverts commit c6e66cba228255316c221ff274d8d1c3a738b8c4.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var passport=require('passport');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login', passport.authenticate('local', { successRedirect: '/users', failureRedirect: '/'}));
 
 module.exports = router;
