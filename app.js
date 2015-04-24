c6e66cba228255316c221ff274d8d1c3a738b8c4
tacticurv <tacticurv@gmail.com>
2015-04-24 05:51:10 +0530

    Revert "PassportJS Integration"
    This reverts commit 0f17defdf06cc1e980091efd0ed6f776c128f33e.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('./cloud/js/auth');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,8 +17,6 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
