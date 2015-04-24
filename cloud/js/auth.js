0f17defdf06cc1e980091efd0ed6f776c128f33e
tacticurv <tacticurv@gmail.com>
2015-04-24 05:50:40 +0530

    PassportJS Integration
    Installed, configured and added PassportJS with admin as user

---
+++ cloud/js/auth.js
@@ -0,0 +1,28 @@
+/**
+ * Created by rupputur on 4/24/2015.
+ */
+
+var passport=require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        return done(null, {username: "admin", password: "password"});
+        //if(username=="admin")
+        //    return done(null, {uasername: "admin", password: "password"});
+        //else if(username!="admin")
+        //    return done(null, false, {message: "The user is not exist"});
+        //else if(password!="password")
+        //    return done(null, false, {message: "Wrong password"});
+    }
+));
+
+passport.serializeUser(function(user,done){
+   done(null, user.username);
+});
+
+passport.deserializeUser(function(username,done){
+    done(null, {username: username});
+});
+
+module.exports=passport;
